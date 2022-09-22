d85f96387baa025eeb36a3ee7e0a4dd19400ee38
Chandan Gupta <me.chandangupta3110@gmail.com>
2022-09-22 20:01:31 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World!</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>
+        </>
     );
 }
 
