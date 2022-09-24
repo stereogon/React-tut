fa706652d33e2e1624294054b99283a167ea11cf
Chandan Gupta <me.chandangupta3110@gmail.com>
2022-09-24 14:29:57 +0530

    Testing Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import NavbarCc from "./components/Navbar-classcomponent";
 
 function App() {
     return (
         <>
             <Navbar />
+            <NavbarCc />
             <div className="App">
                 <h1>Hello World!</h1>
             </div>
